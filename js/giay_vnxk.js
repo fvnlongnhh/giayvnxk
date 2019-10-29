3a936cc74565e8210f32adcc209c1b282bb1c894
Long <30433791+fvnlongnhh@users.noreply.github.com>
2019-10-29 19:03:52 +0700

    top

--- js/giay_vnxk.js
+++ js/giay_vnxk.js
@@ -2,13 +2,43 @@ $(document).ready(function () {
     "use strict";
         $(window).scroll(function() {
         if ($(this).scrollTop() > 0) {
-            $('#btn_top').fadeIn(300);
+            $('.btn_top').fadeIn(300);
         } else {
-            $('#btn_top').fadeOut(300);
+            $('.btn_top').fadeOut(300);
         }
     });
 
-    $('#btn_top').click(function () {
+    $('.btn_top').click(function () {
         $('body,html').animate({scrollTop: 0}, 800);
     });
+
+    $('.slide ul').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+
+    $('.vnxk_box01_r ul').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        speed: 500,
+        pauseOnHover: false,
+        pauseOnFocuse : false,
+        margin: 0,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
 });
+
+
