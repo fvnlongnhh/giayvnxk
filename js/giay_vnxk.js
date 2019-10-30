bcf957322d570a12f36b1b325f3338775db60749
Long <30433791+fvnlongnhh@users.noreply.github.com>
2019-10-30 17:52:35 +0700

    product_page

--- js/giay_vnxk.js
+++ js/giay_vnxk.js
@@ -6,39 +6,44 @@ $(document).ready(function () {
         } else {
             $('.btn_top').fadeOut(300);
         }
+
+        var h_head = $('.head_ct').outerHeight();
+        if ($(this).scrollTop() > 200) {
+            $('.head_ct').addClass('fixed')
+            $('.head_top').css({'margin-bottom': h_head + 'px'});
+
+        } else {
+            $('.head_ct').removeClass('fixed')
+            $('.head_top').css({'margin-bottom': 0});
+        }
+
+        if ($(this).scrollTop() > 300) {
+            $('.head_ct').addClass('active')
+        } else {
+            $('.head_ct').removeClass('active')
+        }
     });
 
     $('.btn_top').click(function () {
         $('body,html').animate({scrollTop: 0}, 800);
     });
 
-    $('.slide ul').slick({
-        dots: false,
-        arrows: true,
-        slidesToShow: 1,
-        speed: 500,
-        pauseOnHover: false,
-        pauseOnFocuse : false,
-        margin: 0,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-        autoplaySpeed: 4000
+    $('.search_btn').click(function(){
+        $('.search_form, body').addClass('active');
     });
 
-    $('.vnxk_box01_r ul').slick({
-        dots: false,
-        arrows: true,
-        slidesToShow: 1,
-        speed: 500,
-        pauseOnHover: false,
-        pauseOnFocuse : false,
-        margin: 0,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-        autoplaySpeed: 4000
+    $('.close_search').click(function(){
+        $('.search_form, body').removeClass('active');
     });
+
+    $('.h_cart').click(function(){
+        $('.cart_form, body').addClass('active');
+    });
+
+    $('.close_cart').click(function(){
+        $('.cart_form, body').removeClass('active');
+    });
+
 });
 
 
